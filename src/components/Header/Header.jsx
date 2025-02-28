a7fb32882667c658a8b23c75af8cacdb02e04244
Vijeth <vijethshetty334@gmail.com>
2025-03-01 00:18:47 +0530

    Make Components Simpler

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import ThemeToggle from './ThemeToggle';
+import NavLinks from './NavLinks';
+import { FaBars } from 'react-icons/fa';
+
+const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    return ( <
+        header >
+        <
+        nav className = "container" >
+        <
+        div >
+        <
+        a href = "#hero" > Vijeth < /a> <
+        /div> <
+        NavLinks isMenuOpen = { isMenuOpen }
+        /> <
+        div >
+        <
+        ThemeToggle / >
+        <
+        FaBars className = "menu-icon"
+        onClick = {
+            () => setIsMenuOpen(!isMenuOpen) }
+        /> <
+        /div> <
+        /nav> <
+        /header>
+    );
+};
+
+export default Header;
