52b4eca86afd641d1aca00d76f6b05820a0f470a
Vijeth <vijethshetty334@gmail.com>
2025-03-02 19:27:39 +0530

    Create Social Links Component for Hero

---
+++ src/components/Hero/SocialLinks.jsx
@@ -0,0 +1,32 @@
+import { FaLinkedin, FaEnvelope, FaGithub } from 'react-icons/fa';
+
+const SocialLinks = ({ links }) => {
+    const getIcon = (name) => {
+        switch (name.toLowerCase()) {
+            case 'linkedin':
+                return <FaLinkedin / > ;
+            case 'email':
+                return <FaEnvelope / > ;
+            case 'github':
+                return <FaGithub / > ;
+            default:
+                return null;
+        }
+    };
+
+    return ( <
+        div className = "social-links" > {
+            links.map((link, index) => ( <
+                a key = { index }
+                href = { link.url }
+                target = "_blank"
+                rel = "noopener noreferrer"
+                ariaLabel = { link.name } > { getIcon(link.name) } <
+                /a>
+            ))
+        } <
+        /div>
+    );
+};
+
+export default SocialLinks;
