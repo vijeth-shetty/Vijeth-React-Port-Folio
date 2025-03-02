e56b4104a35c252f95dc3217cc74d2400010b5cb
Vijeth <vijethshetty334@gmail.com>
2025-03-03 00:12:56 +0530

    Create About Component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,36 @@
+import AboutImage from './AboutImage';
+import { aboutContent } from '../../Data/about';
+
+const About = () => {
+    return ( <
+        section id = "about"
+        className = "container" >
+        <
+        h2 > About Me < /h2> <
+        div className = "about-container" >
+        <
+        AboutImage src = "Resources/Photos/Vijeth.png"
+        alt = "Vijeth Shetty" /
+        >
+        <
+        div className = "about-content" > {
+            aboutContent.map((paragraph, index) => ( <
+                p key = { index } > {
+                    paragraph.map((part, i) => (
+                        part.highlight ? ( <
+                            span key = { i } > { part.text } < /span>
+                        ) : (
+                            part.text
+                        )
+                    ))
+                } <
+                /p>
+            ))
+        } <
+        /div> < /
+        div > <
+        /section>
+    );
+};
+
+export default About;
