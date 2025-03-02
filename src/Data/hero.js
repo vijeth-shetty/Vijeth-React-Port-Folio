3526712e44fc4864219a5e8f752b84419c80cb59
Vijeth <vijethshetty334@gmail.com>
2025-03-02 19:28:30 +0530

    Add Data for Hero

---
+++ src/Data/hero.js
@@ -0,0 +1,30 @@
+export const heroContent = {
+    title: "Hi, I'm Vijeth",
+    subtitle: "Java Full Stack Developer",
+    buttons: [{
+            text: "Contact Me",
+            url: "#contact",
+            download: false,
+            external: false
+        },
+        {
+            text: "Download Resume",
+            url: "/Resources/Files/Vijeth_Resume.pdf",
+            download: true,
+            external: false
+        }
+    ],
+    socialLinks: [{
+            name: "LinkedIn",
+            url: "https://www.linkedin.com/in/vijeth-shetty-334s"
+        },
+        {
+            name: "Email",
+            url: "mailto:vijethshetty334@gmail.com"
+        },
+        {
+            name: "GitHub",
+            url: "https://github.com/vijeth-shetty"
+        }
+    ]
+};
