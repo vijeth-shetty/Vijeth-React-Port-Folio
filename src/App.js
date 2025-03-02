62770c7b3387d042a6cfe88367eadf791d5c898d
Vijeth <vijethshetty334@gmail.com>
2025-03-03 00:15:04 +0530

    Add About Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
         Header / >
         <
         Hero / >
+        <
+        About / >
         <
         /div>
     );
