f50b44398a6224a731c56e0026208d15bb532f36
Vijeth <vijethshetty334@gmail.com>
2025-03-04 00:27:16 +0530

    Add Education Info Data

---
+++ src/Data/education.js
@@ -0,0 +1,25 @@
+export const educationData = [{
+        id: 1,
+        course: "BCA - Computer Applications",
+        college: "Bhandarkars' Arts and Science College, Kundapura, Udupi",
+        university: "Mangalore University",
+        year: "2024",
+        percentage: "85.03%"
+    },
+    {
+        id: 2,
+        course: "PUC - CEBA",
+        college: "VaraSiddhi Vinayaka PU College, Keradi, Udupi",
+        university: "KSEAB",
+        year: "2021",
+        percentage: "95.33%"
+    },
+    {
+        id: 3,
+        course: "SSLC",
+        college: "Sri Mookambika Temple High School, Hosur, Udupi",
+        university: "KSEEB",
+        year: "2019",
+        percentage: "92.64%"
+    }
+];
