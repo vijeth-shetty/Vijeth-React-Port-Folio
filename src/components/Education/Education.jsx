d4f97e3f4ac1ca1bf5f497fc71a24ee52b62ce99
Vijeth <vijethshetty334@gmail.com>
2025-03-04 00:25:53 +0530

    Create Education Component

---
+++ src/components/Education/Education.jsx
@@ -0,0 +1,22 @@
+import EducationItem from './EducationItem';
+import { educationData } from '../../Data/education';
+
+const Education = () => {
+    return ( <
+        section id = "education"
+        className = "container" >
+        <
+        h2 > Education < /h2> <
+        div className = "education-container" > {
+            educationData.map((education) => ( <
+                EducationItem key = { education.id }
+                education = { education }
+                />
+            ))
+        } <
+        /div> < /
+        section >
+    );
+};
+
+export default Education;
