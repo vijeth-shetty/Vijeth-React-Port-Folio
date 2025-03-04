087dde18984eb541ac8c77e5bca3eb5b32035990
Vijeth <vijethshetty334@gmail.com>
2025-03-05 00:35:44 +0530

    Create Skills Data

---
+++ src/Data/skills.js
@@ -0,0 +1,60 @@
+import {
+    FaHtml5,
+    FaCss3Alt,
+    FaBootstrap,
+    FaReact,
+    FaJava,
+    FaNodeJs,
+    FaDatabase,
+    FaNpm,
+    FaGithub,
+    FaGitAlt
+} from 'react-icons/fa';
+import { SiExpress, SiEjs, SiPostgresql, SiMysql, SiIntellijidea } from 'react-icons/si';
+import { DiEclipse } from 'react-icons/di';
+import { TbBrandVscode } from "react-icons/tb";
+
+export const skillsData = {
+    frontend: {
+        title: 'Frontend',
+        skills: [
+            { icon: FaHtml5, name: 'HTML5' },
+            { icon: FaCss3Alt, name: 'CSS3' },
+            { icon: FaBootstrap, name: 'Bootstrap' },
+            { icon: FaReact, name: 'React JS' }
+        ]
+    },
+    backend: {
+        title: 'Backend',
+        skills: [
+            { icon: FaJava, name: 'Java' },
+            { icon: FaNodeJs, name: 'Node.js' },
+            { icon: SiExpress, name: 'Express.js' },
+            { icon: SiEjs, name: 'EJS' }
+        ]
+    },
+    codeEditor: {
+        title: 'Code Editor Tools',
+        skills: [
+            { icon: TbBrandVscode, name: 'VS Code' },
+            { icon: DiEclipse, name: 'Eclipse' },
+            { icon: SiIntellijidea, name: 'Intellij' },
+        ]
+    },
+    database: {
+        title: 'Database',
+        skills: [
+            { icon: FaDatabase, name: 'SQL' },
+            { icon: SiMysql, name: 'MySQL' },
+            { icon: SiPostgresql, name: 'PostgreSQL' }
+        ]
+    },
+    tools: {
+        title: 'Tools & Controls',
+        skills: [
+            { icon: FaNpm, name: 'npm' },
+            { icon: FaGithub, name: 'GitHub' },
+            { icon: FaGitAlt, name: 'Git' },
+        ]
+    }
+};
