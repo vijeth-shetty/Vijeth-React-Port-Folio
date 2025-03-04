af86d04ca94106b58e339634bfc7aea0790f2391
Vijeth <vijethshetty334@gmail.com>
2025-03-05 00:35:02 +0530

    Create Skills List Component

---
+++ src/components/Skills/SkillItem.jsx
@@ -0,0 +1,15 @@
+const SkillItem = ({ icon: Icon, name }) => {
+    return ( <
+        div className = "skill-item" >
+        <
+        div className = "skill-icon" >
+        <
+        Icon ariaHidden = "true" / >
+        <
+        /div> <
+        span className = "skill-name" > { name } < /span> < /
+        div >
+    );
+};
+
+export default SkillItem;
