68121953ea2b52243e0afd3723e3206f031c5c6a
Vijeth <vijethshetty334@gmail.com>
2025-03-05 00:35:24 +0530

    Create Skills Category Component

---
+++ src/components/Skills/SkillCategory.jsx
@@ -0,0 +1,21 @@
+import SkillItem from './SkillItem';
+
+const SkillCategory = ({ title, skills }) => {
+    return ( <
+        div className = "skill-category" >
+        <
+        h3 > { title } < /h3> <
+        div className = "skills-grid" > {
+            skills.map((skill, index) => ( <
+                SkillItem key = { index }
+                icon = { skill.icon }
+                name = { skill.name }
+                />
+            ))
+        } <
+        /div> <
+        /div>
+    );
+};
+
+export default SkillCategory;
