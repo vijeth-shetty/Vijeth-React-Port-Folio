ae6e2b54e13f2190a998782bba22fb89d3fe5632
Vijeth <vijethshetty334@gmail.com>
2025-03-06 01:00:47 +0530

    Create Project Data

---
+++ src/Data/projects.js
@@ -0,0 +1,12 @@
+export const projectsData = [{
+        id: 1,
+        title: "CSS Playground",
+        subtitle: "Interactive Learning & Code Generator",
+        description: "Interactive CSS learning platform with real-time preview",
+        media: "/Resources/Videos/CSS PlayGround.mp4",
+        techStack: ["HTML", "CSS", "JavaScript", "jQuery", "Bootstrap"],
+        demoUrl: "https://css-playground-v2.netlify.app/",
+        codeUrl: "https://github.com/vijeth-shetty/css-playground"
+    },
+    // Add more projects as needed
+];
