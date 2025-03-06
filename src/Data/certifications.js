4c975d829e12c3daf54305bf48f7a2e2885f263c
Vijeth <vijethshetty334@gmail.com>
2025-03-07 00:21:10 +0530

    Add Certification Data

---
+++ src/Data/certifications.js
@@ -0,0 +1,7 @@
+export const certificatesData = [{
+    id: 1,
+    image: "/Resources/Photos/Certificates/Google Cybersecurity.png",
+    title: "Google Cybersecurity Certification",
+    issuer: "Google via Coursera",
+    credentialUrl: "https://www.coursera.org/account/accomplishments/specialization/V5SFU7R5WXJQ"
+}, ];
