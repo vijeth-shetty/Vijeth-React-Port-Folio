446656202aab471c3bf94483c23e73cda81473ce
Vijeth <vijethshetty334@gmail.com>
2025-03-09 00:37:22 +0530

    Add Databases for Developers Foundations Certification

--- src/Data/certifications.js
+++ src/Data/certifications.js
@@ -1,7 +1,14 @@
 export const certificatesData = [{
-    id: 1,
-    image: "/Resources/Photos/Certificates/Google Cybersecurity.png",
-    title: "Google Cybersecurity Certification",
-    issuer: "Google via Coursera",
-    credentialUrl: "https://www.coursera.org/account/accomplishments/specialization/V5SFU7R5WXJQ"
-}, ];
+        id: 1,
+        image: "/Resources/Photos/Certificates/Google Cybersecurity.png",
+        title: "Google Cybersecurity Certification",
+        issuer: "Google via Coursera",
+        credentialUrl: "https://www.coursera.org/account/accomplishments/specialization/V5SFU7R5WXJQ"
+    },
+    {
+        id: 2,
+        image: "/Resources/Photos/Certificates/Databases for Developers Foundations.png",
+        title: "Databases for Developers Foundations",
+        issuer: "Oracle Corporation ( Oracle Dev Gym )"
+    }
+];
