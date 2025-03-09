f5306c9eca48795f9f26d1de7cbfc9e57311816c
Vijeth <vijethshetty334@gmail.com>
2025-03-10 00:22:45 +0530

    Add The Complete Full-Stack Web Development Bootcamp Certificate Data

--- src/Data/certifications.js
+++ src/Data/certifications.js
@@ -10,5 +10,12 @@ export const certificatesData = [{
         image: "/Resources/Photos/Certificates/Databases for Developers Foundations.png",
         title: "Databases for Developers Foundations",
         issuer: "Oracle Corporation ( Oracle Dev Gym )"
+    },
+    {
+        id: 3,
+        image: "/Resources/Photos/Certificates/The Complete Full-Stack Web Development Bootcamp.png",
+        title: "The Complete Full-Stack Web Development Bootcamp",
+        issuer: "Udemy",
+        credentialUrl: 'https://www.udemy.com/certificate/UC-cae89ac2-64f4-4a6a-b442-64b777bdfdfe/'
     }
 ];
