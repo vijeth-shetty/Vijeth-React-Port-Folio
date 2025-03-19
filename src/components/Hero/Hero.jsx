6488671cbbbbc5ffdf12017a8609d2ffc00fa6a6
Vijeth <vijethshetty334@gmail.com>
2025-03-19 13:56:30 +0530

    Update Hero Component Length

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,33 +3,29 @@ import { heroContent } from '../../Data/hero';
 
 
 const Hero = () => {
-    return ( <
-        section id = "hero" >
-        <
-        div className = "container" >
-        <
-        h1 > { heroContent.title } < /h1> <
-        p > { heroContent.subtitle } < /p>
-
-        <
-        div className = "cta-buttons" > {
-            heroContent.buttons.map((button, index) => ( <
-                a key = { index }
-                href = { button.url }
-                className = "btn"
-                download = { button.download }
-                target = { button.external ? "_blank" : undefined }
-                rel = { button.external ? "noopener noreferrer" : undefined } > { button.text } <
-                /a>
-            ))
-        } <
-        /div>
-
-        <
-        SocialLinks links = { heroContent.socialLinks }
-        /> < /
-        div > <
-        /section>
+    return (
+        <section id = "hero" >
+            <div className = "container" >
+                <h1 > { heroContent.title } < /h1>
+                <p > { heroContent.subtitle } < /p>
+                <div className = "cta-buttons" > {
+                    heroContent.buttons.map((button, index) => (
+                        <a
+                            key = { index }
+                            href = { button.url }
+                            className = "btn"
+                            download = { button.download }
+                            target = { button.external ? "_blank" : undefined }
+                            rel = { button.external ? "noopener noreferrer" : undefined }
+                        >
+                            { button.text }
+                        </a>
+                    ))
+                }
+                </div>
+                <SocialLinks links = { heroContent.socialLinks }/>
+            < /div >
+        </section>
     );
 };
 
