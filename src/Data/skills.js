99f766a29b56464ad33988f44ac4c5ae95130a44
Vijeth <vijethshetty334@gmail.com>
2025-03-19 14:00:36 +0530

    Add & Edit Skills Data

--- src/Data/skills.js
+++ src/Data/skills.js
@@ -5,57 +5,64 @@ import {
     FaReact,
     FaJava,
     FaNodeJs,
-    FaDatabase,
     FaNpm,
     FaGithub,
     FaGitAlt
 } from 'react-icons/fa';
-import { SiExpress, SiEjs, SiPostgresql, SiMysql, SiIntellijidea, SiJavascript } from 'react-icons/si';
+import { SiExpress, SiEjs, SiPostgresql, SiMysql, SiIntellijidea, SiJavascript, SiHibernate, SiOracle, SiApachemaven } from 'react-icons/si';
 import { DiEclipse } from 'react-icons/di';
 import { TbBrandVscode } from "react-icons/tb";
 
 export const skillsData = {
+    language: {
+        title: 'Programming & Scripting Languages',
+        skills: [
+            { icon: FaJava, name: 'Java' },
+            { icon: FaJava, name: 'Adv-Java' },
+            { icon: SiJavascript, name: 'JavaScript' },
+        ]
+    },
     frontend: {
-        title: 'Frontend',
+        title: 'Frontend Development ',
         skills: [
             { icon: FaHtml5, name: 'HTML5' },
             { icon: FaCss3Alt, name: 'CSS3' },
-            { icon: SiJavascript, name: 'JavaScript' },
             { icon: FaBootstrap, name: 'Bootstrap' },
-            { icon: FaReact, name: 'React JS' }
-        ]
-    },
-    backend: {
-        title: 'Backend',
-        skills: [
-            { icon: FaJava, name: 'Java' },
-            { icon: FaNodeJs, name: 'Node.js' },
-            { icon: SiExpress, name: 'Express.js' },
+            { icon: FaReact, name: 'React JS' },
             { icon: SiEjs, name: 'EJS' }
         ]
     },
-    codeEditor: {
-        title: 'Code Editor Tools',
+    backend: {
+        title: 'Backend Development',
         skills: [
-            { icon: TbBrandVscode, name: 'VS Code' },
-            { icon: DiEclipse, name: 'Eclipse' },
-            { icon: SiIntellijidea, name: 'Intellij' },
+            { icon: SiHibernate, name: 'Hibernate' },
+            { icon: FaNodeJs, name: 'Node.js' },
+            { icon: SiExpress, name: 'Express.js' },
         ]
     },
     database: {
-        title: 'Database',
+        title: 'Databases & SQL',
         skills: [
-            { icon: FaDatabase, name: 'SQL' },
+            { icon: SiOracle, name: 'Oracle SQL' },
             { icon: SiMysql, name: 'MySQL' },
             { icon: SiPostgresql, name: 'PostgreSQL' }
         ]
     },
-    tools: {
-        title: 'Tools & Controls',
+    codeEditor: {
+        title: 'Development Tools & IDEs',
         skills: [
-            { icon: FaNpm, name: 'npm' },
-            { icon: FaGithub, name: 'GitHub' },
+            { icon: TbBrandVscode, name: 'VS Code' },
+            { icon: DiEclipse, name: 'Eclipse' },
+            { icon: SiIntellijidea, name: 'Intellij IDEA' },
+        ]
+    },
+    tools: {
+        title: 'Version Control & Package Management',
+        skills: [
             { icon: FaGitAlt, name: 'Git' },
+            { icon: FaGithub, name: 'GitHub' },
+            { icon: FaNpm, name: 'npm' },
+            { icon: SiApachemaven, name: 'Apache Maven' },
         ]
     }
 };
