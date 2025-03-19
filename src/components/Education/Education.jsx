9721440570a286abdc1900a40914c6a58f595072
Vijeth <vijethshetty334@gmail.com>
2025-03-19 13:55:09 +0530

    Update Education Component Length

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -2,20 +2,19 @@ import EducationItem from './EducationItem';
 import { educationData } from '../../Data/education';
 
 const Education = () => {
-    return ( <
-        section id = "education"
-        className = "container" >
-        <
-        h2 > Education < /h2> <
-        div className = "education-container" > {
-            educationData.map((education) => ( <
-                EducationItem key = { education.id }
-                education = { education }
-                />
-            ))
-        } <
-        /div> < /
-        section >
+    return (
+        <section id = "education" className = "container" >
+            <h2 > Education < /h2>
+            <div className = "education-container" > {
+                educationData.map((education) => (
+                    <EducationItem
+                        key = { education.id }
+                        education = { education }
+                    />
+                ))
+            }
+            </div>
+        </section >
     );
 };
 
