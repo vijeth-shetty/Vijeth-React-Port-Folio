fbbbb19ee09ec26ec939db0354ea8dc67a99829c
Vijeth <vijethshetty334@gmail.com>
2025-03-19 13:57:30 +0530

    Update Project Component Length

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,20 +2,19 @@ import ProjectCard from './ProjectCard';
 import { projectsData } from '../../Data/projects';
 
 const Projects = () => {
-    return ( <
-        section id = "projects"
-        className = "container" >
-        <
-        h2 > Projects < /h2> <
-        div className = "project-grid" > {
-            projectsData.map((project) => ( <
-                ProjectCard key = { project.id }
-                project = { project }
-                />
-            ))
-        } <
-        /div> < /
-        section >
+    return (
+        <section id = "projects" className = "container" >
+            <h2 > Projects < /h2>
+            <div className = "project-grid" > {
+                projectsData.map((project) => (
+                    <ProjectCard
+                        key = { project.id }
+                        project = { project }
+                    />
+                ))
+            }
+            </div>
+        < /section >
     );
 };
 
