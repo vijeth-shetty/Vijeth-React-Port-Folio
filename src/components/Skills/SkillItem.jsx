9ab5feab623992a2652a9c2c1c6685079f87939c
Vijeth <vijethshetty334@gmail.com>
2025-03-19 13:58:16 +0530

    Update Skill Items Component Length

--- src/components/Skills/SkillItem.jsx
+++ src/components/Skills/SkillItem.jsx
@@ -1,14 +1,11 @@
 const SkillItem = ({ icon: Icon, name }) => {
-    return ( <
-        div className = "skill-item" >
-        <
-        div className = "skill-icon" >
-        <
-        Icon ariaHidden = "true" / >
-        <
-        /div> <
-        span className = "skill-name" > { name } < /span> < /
-        div >
+    return (
+        <div className = "skill-item" >
+            <div className = "skill-icon" >
+                <Icon ariaHidden = "true" />
+            </div>
+            <span className = "skill-name" > { name } < /span>
+        < /div >
     );
 };
 
